789167cf4458d08777f3a41b1db47172df426816
Alexander Smith <ajsmith@acm.org>
2022-03-27 14:56:20 -0400

    Add install script.

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+PACKAGE_ROOT=$(cd $(dirname $0); pwd)
+
+if [[ !(-d venv) ]]
+then
+    python3 -m venv venv
+fi
+
+source venv/bin/activate
+
+cd ${PACKAGE_ROOT}
+pip install -r requirements.txt
+pip install -e .
